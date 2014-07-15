446d88ff4738d1af779937c7c3db5fbf690664a0
Lia Kitamura <kit4mura@gmail.com>
2014-07-14 20:44:39 -0600

    modified routes to show new beer info

--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,24 @@ Rails.application.routes.draw do
     resources :beers
   end
 
-  resources :beers, only: [:index, :show] do
-    collection do
-      get '/categories', to: 'beers#index'
-    end
-  end
+  resources :categories, only: [:index, :show]
+  resources :styles, only: [:show]
+  resources :beers, only: [:index, :show]
 
-  resources :online_brews, path: 'b', only: [:index] do
-    collection do
-      get '/beer/:id', to: 'online_brews#show_beer', as: :show_beer
-    end
-  end
+  # resources :categories, only: [:index, :show] do
+  #   get '/styles/:id', to: 'styles#index'
+  # end
+
+  # resources :online_brews, path: 'b', only: [:index] do
+  #   collection do
+  #     get '/beer/:id', to: 'online_brews#show_beer', as: :show_beer
+  #   end
+  # end
+
+  # resources :beers, only: [:index, :show] do
+  #   collection do
+  #     get '/categories', to: 'beers#index'
+  #   end
+  # end
 
 end
