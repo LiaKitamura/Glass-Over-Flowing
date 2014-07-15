a25587fc967a33e5e813cdf30d207a8097395474
Lia Kitamura <kit4mura@gmail.com>
2014-07-14 21:53:28 -0600

    removed conflicting column from beer model and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715003646) do
+ActiveRecord::Schema.define(version: 20140715031715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20140715003646) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "style_id"
-    t.integer  "resources_style_id"
     t.text     "beer_description"
   end
 
