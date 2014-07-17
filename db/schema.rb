dd753a54f7e9d0f27e8b933372e37558d16a5c55
Justin Veirs <justinveirs@gmail.com>
2014-07-16 21:55:30 -0600

    Merge branch 'inception'
    Conflicts:
    	Gemfile
    	Gemfile.lock
    	app/assets/stylesheets/styles.css.scss
    	app/controllers/beers_controller.rb
    	app/controllers/categories_controller.rb
    	app/controllers/styles_controller.rb
    	app/models/beer.rb
    	app/models/style.rb
    	app/models/user.rb
    	app/views/beers/show.html.erb
    	app/views/categories/index.html.erb
    	app/views/layouts/_navbar.html.erb
    	app/views/layouts/application.html.erb
    	app/views/styles/show.html.erb
    	config/routes.rb
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140715031715) do
-=======
 ActiveRecord::Schema.define(version: 20140716003858) do
->>>>>>> inception
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,18 +26,7 @@ ActiveRecord::Schema.define(version: 20140716003858) do
     t.datetime "updated_at"
     t.integer  "style_id"
     t.text     "beer_description"
-<<<<<<< HEAD
-  end
-
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "resources_category_id"
-    t.text     "category_description"
-=======
     t.string   "slug"
->>>>>>> inception
   end
 
   add_index "beers", ["slug"], name: "index_beers_on_slug", unique: true, using: :btree
@@ -67,8 +52,6 @@ ActiveRecord::Schema.define(version: 20140716003858) do
 
   add_index "favorites", ["favorable_id", "favorable_type"], name: "index_favorites_on_favorable_id_and_favorable_type", using: :btree
 
-<<<<<<< HEAD
-=======
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
     t.integer  "sluggable_id",              null: false
@@ -82,7 +65,6 @@ ActiveRecord::Schema.define(version: 20140716003858) do
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
->>>>>>> inception
   create_table "styles", force: true do |t|
     t.string   "style_name"
     t.string   "category_name"
@@ -91,17 +73,11 @@ ActiveRecord::Schema.define(version: 20140716003858) do
     t.integer  "category_id"
     t.integer  "resources_id"
     t.text     "style_description"
-<<<<<<< HEAD
-  end
-
-  add_index "styles", ["category_id"], name: "index_styles_on_category_id", using: :btree
-=======
     t.string   "slug"
   end
 
   add_index "styles", ["category_id"], name: "index_styles_on_category_id", using: :btree
   add_index "styles", ["slug"], name: "index_styles_on_slug", unique: true, using: :btree
->>>>>>> inception
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
