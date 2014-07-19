752d011b5328794a3924a1f8c63784502c7f8e46
Lia Kitamura <kit4mura@gmail.com>
2014-07-19 17:23:04 -0600

    fixed search

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,7 +1,8 @@
 class SearchResultsController < ApplicationController
 
   def index
-    @results = Style.search(params[:q]).collect(&:searchable)
+    # @results = Style.search(params[:q]).collect(&:searchable)
+    @results = Style.search(params[:q])
   end
 
 end
