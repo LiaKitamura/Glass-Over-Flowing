c0af9bb322f07fc30b0be745986af8f3f129c3fc
Justin Veirs <justinveirs@gmail.com>
2014-07-19 11:37:17 -0600

    making heroku happy

--- config/application.rb
+++ config/application.rb
@@ -5,10 +5,11 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 
 module GlassOverFlowing
   class Application < Rails::Application
+  config.assets.initialize_on_precompile = false
   config.generators do | g |
      g.test_framework :rspec
    end
