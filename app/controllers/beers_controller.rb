f61b3bdd2e007dfede71a83f49b955e17ec41162
Justin Veirs <justinveirs@gmail.com>
2014-07-26 13:44:32 -0600

    testing, or in the beginning stages

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,21 +1,7 @@
 class BeersController < ApplicationController
 
-  def index
-    @brews = Beer.all
-  end
-
   def show
     @brew = Beer.friendly.find(params[:id])
   end
 
-  def abv
-    # find @brews abv where its not nil
-    
-  end
-
-  private
-
-  def brew_params
-    params.require(:beer).permit(:category, :style, :name, :brewed_by, :abv)
-  end
 end
