cadfbf68c909a5181acf50dbdbb2468565e3bb2a
Justin Veirs <justinveirs@gmail.com>
2014-07-26 17:50:52 -0600

    trying to complete the test

--- spec/controllers/beers_controller_spec.rb
+++ spec/controllers/beers_controller_spec.rb
@@ -4,18 +4,13 @@ RSpec.describe BeersController, :type => :controller do
 
     describe '#show', :focus do
       before do
-        @brew = create(:beers, :category_name, 'British Origin Ale', :style_description, 'Brown Porter')
-        sign_in @user
+        @brew = create(:beer, category: 'South German-Style Hefeweizen / Hefeweissbier', style: 'A Slice of Hefen')
       end
       it 'displays beer on show page' do
         get :show, id: @brew.id
         expect(response).to be_success
-        expect(assigns(:beers).category_name).to eq 'Brown Porter'
+        expect(assigns(:beer).category).to eq 'South German-Style Hefeweizen / Hefeweissbier'
         expect(response).to render_template('show')
       end
     end
-
-
-
-
 end
