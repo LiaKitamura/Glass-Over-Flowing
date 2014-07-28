c21c4f16d1b3eade07b8c7bbb38d7334423af8d6
Justin Veirs <justinveirs@gmail.com>
2014-07-28 16:31:18 -0600

    house cleaning

--- spec/controllers/beers_controller_spec.rb
+++ spec/controllers/beers_controller_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe BeersController, :type => :controller do
   describe '#show' do
     before do
-      # @category = create(:category, name: "South German-Style Hefeweizen / Hefeweissbier")
-      # @style = create(:style, style_name: "A Slice of Hefen")
       @brew = create(:beer, name: "In Heat Wheat")
     end
     it 'displays beer on show page' do
