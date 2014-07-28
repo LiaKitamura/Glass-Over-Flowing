4875f6562d7cbc0c5433848f4407acdb313e38a4
Justin Veirs <justinveirs@gmail.com>
2014-07-28 14:44:35 -0600

    user assocaitons and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,26 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-  # describe "associations" do
-  #   it { should have_many :favorites }
-  # end
-  #
-  # describe "validations" do
-  #   it { should validate_presence_of :name }
-  #   it { should validate_presence_of :email }
-  # end
-  #
-  # describe '#name' do
-  #   before do
-  #     @user = create(:user, name: 'Justin', email: 'test@test.com', password: 'password1')
-  #   end
-  #   it 'takes user and returns Justin' do
-  #     expect(@user.name).to eq 'Justin'
-  #   end
-  # end
-  #
+  describe "associations" do
+    it { should have_many :favorites }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+  end
+
+  describe '#name' do
+    before do
+      @user = create(:user, name: "Justin", email: "test@test.com", password: "password1")
+    end
+    it 'takes user and returns Justin' do
+      expect(@user.name).to eq "Justin"
+      expect(@user.email).to eq "test@test.com"
+      expect(@user.password).to eq "password1"
+    end
+  end
+
   # describe '#email' do
   #   before do
   #     @user = create(:user, email: 'test@test.com')
