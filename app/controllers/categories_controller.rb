6fdcc492a20f9ddc96061e58ed52d8da5f3ddb5b
Lia Kitamura <kit4mura@gmail.com>
2014-07-28 15:46:18 -0600

    just about finished with styling

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
-  def grouped
-    @grouped = @categories.in_groups_of(7, false)
-  end
+  # def grouped
+  #   @grouped = @categories.in_groups_of(7, false)
+  # end
 end
