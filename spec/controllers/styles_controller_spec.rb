b21f47d7c7af199234ac7b521a1ac760556dfbba
Justin Veirs <justinveirs@gmail.com>
2014-07-28 15:41:03 -0600

    specs

--- spec/controllers/styles_controller_spec.rb
+++ spec/controllers/styles_controller_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe StylesController, :type => :controller do
 
-  # describe "#show", :focus do
-  #   before do
-  #     @style = create(:style)
-  #   end
-  #   it "displays styles" do
-  #     get :show, @style.style_name
-  #     expect(Style.count).to eq 'Pale Ale'
-  #   end
-  # end
-
+  describe '#show', :focus do
+    before do
+      @style = create(:style, name: "Hipster")
+    end
+    it 'displays styles' do
+      get :show, id: @style.id
+      expect(response).to be_success
+      expect(@sytle.name).to eq "Hipster"
+      expect(response).to render_template('show')
+    end
+  end
 end
