94cdbcad9bea928a7c47fb354d81a72e3d202e04
Justin Veirs <justinveirs@gmail.com>
2014-07-28 16:25:11 -0600

    running specs on styles

--- spec/controllers/styles_controller_spec.rb
+++ spec/controllers/styles_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe StylesController, :type => :controller do
 
-  describe '#show', :focus do
+  describe '#show' do
     before do
-      @style = create(:style, name: "Hipster")
+      @style = create(:style, style_name: "Pale Ale")
     end
     it 'displays styles' do
       get :show, id: @style.id
       expect(response).to be_success
-      expect(@sytle.name).to eq "Hipster"
+      expect(@style.style_name).to eq "Pale Ale"
       expect(response).to render_template('show')
     end
   end
