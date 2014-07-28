6d9276cbacd39538a0327211dde0d3e6bc9d7773
Justin Veirs <justinveirs@gmail.com>
2014-07-28 11:05:57 -0600

    specs

--- spec/controllers/beers_controller_spec.rb
+++ spec/controllers/beers_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe BeersController, :type => :controller do
   describe '#show' do
     before do
-      @category = create(:category, name: "South German-Style Hefeweizen / Hefeweissbier")
-      @style = create(:style, style_name: "A Slice of Hefen")
-      @brew = create(:beer, category: @category, style: @style)
+      # @category = create(:category, name: "South German-Style Hefeweizen / Hefeweissbier")
+      # @style = create(:style, style_name: "A Slice of Hefen")
+      @brew = Beer.create(:beer, name: "In Heat Wheat")
     end
     it 'displays beer on show page' do
       get :show, id: @brew.id
       expect(response).to be_success
-      expect(@brew.category.name).to eq 'South German-Style Hefeweizen / Hefeweissbier'
+      expect(@brew.name).to eq "In Heat Wheat"
       expect(response).to render_template('show')
     end
   end
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, :type => :controller do
-  describe '#index', :focus do
+  describe '#index' do
     before do
       @categories = create(:category, name: "cheeze")
     end
     it 'displays a collection of categories' do
       get :index
       expect(response).to be_success
-      expect(assigns(:category).name).to eq "cheeze"
+      expect(assigns(@category).name).to eq "cheeze"
       expect(response).to render_template('index')
     end
-    
+
   end
 end
