8b1f729fe6804caa42bcad547e0118ec6329e356
Lia Kitamura <kit4mura@gmail.com>
2014-07-30 11:56:30 -0600

    cleaning up css pages getting them to be more uniform

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -4,12 +4,9 @@ class BeersController < ApplicationController
     @brew = Beer.friendly.find(params[:id])
   end
 
-<<<<<<< HEAD
-=======
   private
 
   def brew_params
-    params.require(:beer).permit(:category, :style, :name, :brewed_by, :abv)
+    params.require(:beer).permit(:name)
   end
->>>>>>> 5926a894cf9040c199f7fdb773095d9f8207e8c9
 end
--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -2,8 +2,8 @@ class SearchResultsController < ApplicationController
 
   def index
     # @beers = PgSearch.multisearch(params[:q]).collect(&:searchable)
-    @beers = PgSearch.multisearch(params[:q]).where(:searchable_type => "Beer").limit(5).collect(&:searchable)
+    @beers = PgSearch.multisearch(params[:q]).where(:searchable_type => "Beer").collect(&:searchable)
     # @styles = PgSearch.multisearch(params[:q]).collect(&:searchable)
-    @styles = PgSearch.multisearch(params[:q]).where(:searchable_type => "Style").limit(5).collect(&:searchable)
+    @styles = PgSearch.multisearch(params[:q]).where(:searchable_type => "Style").collect(&:searchable)
   end
 end
