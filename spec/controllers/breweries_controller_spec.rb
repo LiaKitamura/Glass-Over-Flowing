0b584ee2ff02c20065f7caa495325344567fd78a
Justin Veirs <justinveirs@gmail.com>
2014-08-01 12:21:33 -0600

    final spec to reach 80%

--- spec/controllers/breweries_controller_spec.rb
+++ spec/controllers/breweries_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe BreweriesController, :type => :controller do
-
+  describe '#show' do
+    before do
+      @brewery = create(:brewery, brewery_name: "Yo")
+    end
+    it 'displays a brewery on the page' do
+      get :show, id: @brewery.id
+      expect(response).to be_success
+      expect(@brewery.brewery_name).to eq "Yo"
+      expect(response).to render_template('show')
+    end
+  end
 end
--- spec/factories/breweries.rb
+++ spec/factories/breweries.rb
@@ -3,9 +3,5 @@
 FactoryGirl.define do
   factory :brewery do
     brewery_name { Faker::Name.brewery_name }
-    brewery_description { Faker::Lorem.sentence }
-    website { Faker::Internet.domain_name }
-    icon { Faker::Name.icon }
-    large_image { Faker::Internet.image }
   end
 end
