638a74896f8c6d4740cba95248274fedf6bc259f
Lia Kitamura <kit4mura@gmail.com>
2014-10-15 07:13:10 -0600

    changed Glass-Over-Flowing to use _ instead. having db issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_round_3_session'
+Rails.application.config.session_store :cookie_store, key: '_glass_over_flowing_session'
